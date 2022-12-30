dc109a26db94be32a07fe25580976017cc55b6a0
gawasa <yusei112929@gmail.com>
2022-12-30 17:19:33 +0900

    add timerState

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Timer? countdownTimer;
+  bool timerState = false;
+
   //自分のタイマーの時間の実体
-  Duration myDuration = Duration(hours: 80);
+  Duration myDuration = const Duration(hours: 80);
   @override
   void initState() {
     super.initState();
@@ -42,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void startTimer() {
     countdownTimer =
-        Timer.periodic(Duration(seconds: 1), (_) => setCountDown());
+        Timer.periodic(const Duration(seconds: 1), (_) => setCountDown());
   }
 
   void pauseTimer() {
@@ -51,11 +53,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void resetTimer() {
     pauseTimer();
-    setState(() => myDuration = Duration(hours: 80));
+    setState(() => myDuration = const Duration(hours: 80));
   }
 
   void setCountDown() {
-    final reduceSecondsBy = 1;
+    const reduceSecondsBy = 1;
     setState(() {
       //一秒引く
       final seconds = myDuration.inSeconds - reduceSecondsBy;
@@ -80,24 +82,31 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            IconButton(onPressed: startTimer, icon: Icon(Icons.play_arrow)),
-            IconButton(
-                onPressed: () {
-                  if (countdownTimer == null || countdownTimer!.isActive) {
-                    pauseTimer();
-                  }
-                },
-                icon: Icon(Icons.pause)),
+            (timerState)
+                ? IconButton(
+                    onPressed: () {
+                      if (countdownTimer == null || countdownTimer!.isActive) {
+                        pauseTimer();
+                        timerState = false;
+                      }
+                    },
+                    icon: const Icon(Icons.pause))
+                : IconButton(
+                    onPressed: () {
+                      startTimer();
+                      timerState = true;
+                    },
+                    icon: const Icon(Icons.play_arrow)),
             Text(
               '$hours:$minutes:$seconds',
-              style: TextStyle(fontSize: 60),
+              style: const TextStyle(fontSize: 60),
             ),
             IconButton(
                 onPressed: () {
                   resetTimer();
+                  timerState = false;
                 },
-                icon: Icon(Icons.restart_alt)),
-            // Step 11
+                icon: const Icon(Icons.restart_alt)),
           ],
         ),
       ),
